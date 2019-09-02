403032a0e61f4fe9a8851c21518d49ed2167929d
taiyeeba <taiyeeba.chikhalia@gmail.com>
2019-09-02 21:12:21 +0530

    tryig comments import

--- standard_import.py
+++ standard_import.py
@@ -2,6 +2,18 @@ f = open("codes\\clsfun.py")
 lineNo = 0
 a = list(map(str,f))
 for i in a:
+    a[i] = a.strip()
+    if a[i][0]=="\"\"\"":
+        lineNo += 1
+        while "\"\"\"" not in a[i]:
+            lineNo += 1
+        continue
+    elif a[i][0] == "\'\'\'":
+        while "\'\'\'" not in a[i]:
+            lineNo += 1
+        continue
+    elif a[i][0] == "#":
+        continue
     if 'import' not in i:
         break
     lineNo += 1
