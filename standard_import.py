f91a7d51c384412e4a51591d36fb26efd8330071
Neha Rane <47265630+neha-rane@users.noreply.github.com>
2019-09-27 12:17:17 +0530

    Update standard_import.py

--- standard_import.py
+++ standard_import.py
@@ -1,10 +1,63 @@
-f = open("testcases\\clsfun.py")
+f = open("codes/clsfun.py")
 lineNo = 0
 a = list(map(str,f))
-for i in a:
-    if 'import' not in i:
+
+i = 0
+
+#for i in range(len(a)):
+while i<len(a):
+    temp = a[i]
+    a[i] = temp.strip()
+    if a[i]=="":
+        i+=1
+        continue
+    elif a[i][0] =="\"" and a[i][1] =="\"" and a[i][2] =="\"":
+        lineNo += 1
+        i += 1
+        while "\"\"\"" not in a[i]:
+            lineNo += 1
+            i += 1
+        i += 1
+        continue
+    elif a[i][0] == "\'" and a[i][1] == "\'" and a[i][2] == "\'":
+        i += 1        
+        while "\'\'\'" not in a[i]:
+            lineNo += 1
+            i += 1
+        i+= 1
+        continue
+    elif a[i][0] == "#":
+        continue
+    if 'import' not in a[i]:
         break
     lineNo += 1
-for i in range(lineNo, len(a)):
-    if 'import' in a[i]:
+    i += 1
+
+
+while i<len(a):
+    temp = a[i]
+    a[i] = temp.strip()
+    if a[i]=="":
+        i+=1
+        continue
+    elif a[i][0] =="\"" and a[i][1] =="\"" and a[i][2] =="\"":
+        lineNo += 1
+        i += 1
+        while "\"\"\"" not in a[i]:
+            lineNo += 1
+            i += 1
+        i+=1
+        continue
+    elif a[i][0] == "\'" and a[i][1] == "\'" and a[i][2] == "\'":
+        i += 1
+        
+        while "\'\'\'" not in a[i]:
+            lineNo += 1
+            i += 1
+        i+= 1
+        continue
+    elif a[i][0] == "#":
+        continue
+    if 'import' in a[i] and ('#' not in a[i] and '"' not in a[i]):
         print("Import Statement placed wrongly on line number: ",i+1)
+    i += 1
